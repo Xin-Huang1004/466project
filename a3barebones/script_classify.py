e9ce96a29b07d5b2cd2c1a218c15f7c50e2ec5ff
pang <383066079@qq.com>
2017-11-27 16:53:59 -0700

    pull

--- a3barebones/script_classify.py
+++ a3barebones/script_classify.py
@@ -7,6 +7,9 @@ import numpy as np
 import dataloader as dtl
 import classalgorithms as algs
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
 
 def getaccuracy(ytest, predictions):
     correct = 0
@@ -19,8 +22,16 @@ def geterror(ytest, predictions):
     return (100.0-getaccuracy(ytest, predictions))
 
 
+def randrange(n, vmin, vmax):
+    '''
+    Helper function to make an array of random numbers having shape (n, )
+    with each number distributed Uniform(vmin, vmax).
+    '''
+    return (vmax - vmin)*np.random.rand(n) + vmin
+
+
 if __name__ == '__main__':
-    trainsize = 10000   
+    trainsize = 10000
     testsize =  10000
     numruns = 2
 
@@ -28,9 +39,9 @@ if __name__ == '__main__':
                  #'Naive Bayes': algs.NaiveBayes({'usecolumnones': False}),
                  #'Naive Bayes Ones': algs.NaiveBayes({'usecolumnones': True}),
                  'Linear Regression': algs.LinearRegressionClass(),
-                 'Logistic Regression': algs.LogitReg(),
-                # 'Neural Network': algs.NeuralNet({'epochs': 100}),
-                 
+                 #'Logistic Regression': algs.LogitReg(),
+                 #'Neural Network': algs.NeuralNet({'epochs': 100}),
+
                 }
     numalgs = len(classalgs)
 
@@ -48,8 +59,8 @@ if __name__ == '__main__':
 
     for r in range(numruns):
         trainset, testset = dtl.load_skin(trainsize,testsize,r)
-        print trainset
-        
+        print (trainset)
+
         print(('Running on train={0} and test={1} samples for run {2}').format(trainset[0].shape[0], testset[0].shape[0],r))
 
         for p in range(numparams):
@@ -80,10 +91,26 @@ if __name__ == '__main__':
         learner.reset(parameters[bestparams])
         print("")
         print ('Best parameters for ' + learnername + ': ' + str(learner.getparams()))
-        print ('Average error for ' + learnername + ': ' + str(besterror) + ' +- ' + str(np.std(errors[learnername][bestparams,:])/math.sqrt(numruns)))
+        #print ('Average error for ' + learnername + ': ' + str(besterror) + ' +- ' + str(np.std(errors[learnername][bestparams,:])/math.sqrt(numruns)))
     for i,j in classalgs.items():
-        print i + ":"
+        #print i + ":"
         errorList = errors[i][0]
-        print errorList
+        print (errorList)
         #sample_std = np.std(errors[learnername],ddof=1) / np.sqrt(numparams)
         #print("Standard error for " +learnername + ": " + str(sample_std))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for c, m, zlow, zhigh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
+        xs = randrange(n, 23, 32)
+        ys = randrange(n, 0, 100)
+        zs = randrange(n, zlow, zhigh)
+        ax.scatter(xs, ys, zs, c=c, marker=m)
+
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
